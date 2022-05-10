73514ea23bd997fd62edc0bc4842357be34c9588
bigolboyyo <wesley.grant94@gmail.com>
2022-05-10 19:16:02 -0400

    Nighttime commit

--- scripts.js
+++ scripts.js
@@ -26,7 +26,7 @@ header.append(h1);
 
 let p1 = document.createElement("p");
 p1.id = "p1";
-p1.textContent = "Enter City Name Below";
+p1.textContent = "Enter location for today's weather!";
 p1.style.textAlign = "center";
 header.append(p1);
 
@@ -64,6 +64,7 @@ function fetchWeather(e) {
 }
 
 function weatherSearch(e) {
+  cityInput.value = "";
   let weatherKeys = Object.keys(e.currentConditions);
   let weatherValues = Object.values(e.currentConditions);
   let temps = Object.values(weatherValues[1]);
@@ -73,18 +74,33 @@ function weatherSearch(e) {
   card.id = `${userInput}-card`;
   card.className = "cards";
 
-  let isFull = container.childElementCount >= 2;
-
-  card.innerHTML = `
- <h1>${userInput.toUpperCase()}</h1>
- <p>Time: ${weatherValues[0]}</p>
- <li id="comment">Comment: ${weatherValues[6]}</li>
- <li id="precipitation">Precipitation: ${weatherValues[2]}
-     <li id="temp">Temp: ${temps[0]}c | ${temps[1]}f</li>
-     <li id="humidity">Humidity: ${weatherValues[3]}</li>
-     <li id="wind">Wind Speed: ${speeds[0]}km | ${speeds[1]}mile</li>
-     `;
   container.append(card);
+
+  const front = document.createElement("div");
+  front.id = `${userInput}-front`;
+  front.className = "front";
+
+  front.innerHTML = `
+  <h1>${userInput.toUpperCase()}</h1>
+  <p>Time: ${weatherValues[0]}</p>
+  <li id="comment">Comment: ${weatherValues[6]}</li>
+  <li id="precipitation">Precipitation: ${weatherValues[2]}
+  <li id="temp">Temp: ${temps[0]}c | ${temps[1]}f</li>
+  <li id="humidity">Humidity: ${weatherValues[3]}</li>
+  <li id="wind">Wind Speed: ${speeds[0]}km | ${speeds[1]}mile</li>
+  `;
+  card.append(front);
+
+  const back = document.createElement("div");
+  back.id = `${userInput}-back`;
+  back.className = "back";
+
+  back.innerHTML = `
+  <textarea id="back-text" name="backtext"
+  rows="10" cols="33">
+  </textarea>
+  `;
+  //card.append(back);
 }
 
 document.addEventListener("DOMContentLoaded", (e) => {
