774be3dcb1a77be075628f1eb34edd45f6c360f3
bigolboyyo <wesley.grant94@gmail.com>
2022-05-11 18:27:04 -0400

    5/11/22

--- scripts.js
+++ scripts.js
@@ -1,3 +1,9 @@
+const body = document.getElementsByTagName("body");
+
+const unsplashRandom =
+  "https://source.unsplash.com/random/1920x1080/?landscapes";
+document.body.style.backgroundImage = `url(${unsplashRandom})`;
+
 const header = document.createElement("div");
 header.className = "header";
 header.id = "header";
@@ -9,7 +15,7 @@ document.body.append(container);
 
 const h1 = document.createElement("h1");
 h1.id = "h1";
-h1.textContent = "Weather Search SPA";
+h1.textContent = "WeatherDB Search";
 h1.style.textAlign = "center";
 header.append(h1);
 
@@ -22,13 +28,13 @@ header.append(p1);
 const cityInput = document.createElement("input");
 cityInput.className = "search-inputs";
 cityInput.id = "city-input";
-cityInput.placeholder = "ex: New York, Toronto, London";
+cityInput.placeholder = " ex: New York, Toronto, London";
 header.append(cityInput);
 
 const searchButton = document.createElement("button");
 searchButton.className = "buttons";
 searchButton.id = "search-btn";
-searchButton.textContent = "Search";
+searchButton.textContent = "🔍";
 
 let br = document.createElement("br");
 header.append(br);
@@ -36,7 +42,7 @@ header.append(br);
 header.append(searchButton);
 
 function fetchWeather(e) {
-  userInput = cityInput.value;
+  let userInput = cityInput.value;
 
   let config = {
     Headers: {
@@ -49,10 +55,16 @@ function fetchWeather(e) {
     config
   )
     .then((res) => res.json())
-    .then((json) => weatherSearch(json));
+    .then((json) => weatherSearch(json, userInput))
+    .catch((err) => {
+      window.alert(
+        `${err} \n https://weatherdbi.herokuapp.com/data/weather/${userInput}`
+      );
+      console.log(err);
+    });
 }
 
-function weatherSearch(e) {
+function weatherSearch(e, userInput) {
   cityInput.value = "";
   let weatherKeys = Object.keys(e.currentConditions);
   let weatherValues = Object.values(e.currentConditions);
@@ -77,7 +89,10 @@ function weatherSearch(e) {
   <li id="temp">Temp: ${temps[0]}c | ${temps[1]}f</li>
   <li id="humidity">Humidity: ${weatherValues[3]}</li>
   <li id="wind">Wind Speed: ${speeds[0]}km | ${speeds[1]}mile</li>
+  </br>
+  <button id="notes-btn" class="buttons">Notes</button>
   `;
+
   card.append(front);
 
   const back = document.createElement("div");
@@ -86,19 +101,35 @@ function weatherSearch(e) {
 
   back.innerHTML = `
   <textarea id="back-text" name="backtext"
-  rows="10" cols="33">
+  rows="25" placeholder="NotePad">
   </textarea>
+  </br>
+  <button id="front-btn">Flip</button>
   `;
-  //card.append(back);
+
+  let notesBtn = document.getElementById("notes-btn");
+  notesBtn.addEventListener("click", handleFrontClick);
+  console.log(notesBtn);
+
+  function handleFrontClick(e) {
+    e.target.parentNode.remove();
+    card.appendChild(back);
+
+    let frontBtn = document.getElementById("front-btn");
+    frontBtn.addEventListener("click", (e) => {
+      e.target.parentNode.remove();
+      card.appendChild(front);
+    });
+  }
 }
 
 document.addEventListener("DOMContentLoaded", (e) => {
   searchButton.addEventListener("click", (e) => {
-    e.preventDefault();
     fetchWeather();
   });
+
   document.addEventListener("keydown", function (e) {
-    if (e.key === "Enter") {
+    if (e.key === "Enter" && cityInput.value.length > 0) {
       fetchWeather();
     }
   });
